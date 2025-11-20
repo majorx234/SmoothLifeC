d06b728bb8b2dc8e8422d1cef0fd23b7ad559a48
MajorX234 <majorx234@googlemail.com>
2025-11-20 22:06:41 +0100

    Add first test for clamp2 function

--- src/tests.c
+++ src/tests.c
@@ -1,6 +1,13 @@
 #include <stddef.h>
 #include <stdio.h>
+#include <assert.h>
+#include "smooth_life.h"
 
 int main(int argc, char **argv) {
   printf("tests\n");
+}
+
+bool test_clamp2() {
+  assert(clamp2(5.0, 0.0, 1.0) == 1.0);
+  printf("All clamp tests passed.\n");
 }
