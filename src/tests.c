930d9ca4e83464a66627f0b198534c2cb0d06638
MajorX234 <majorx234@googlemail.com>
2025-11-21 23:15:38 +0100

    Add test for linearized threshold

--- src/tests.c
+++ src/tests.c
@@ -20,7 +20,7 @@ bool test_logistic_threshold() {
   double x0 = 0.0;
   double alpha = 6.0;
   logistic_threshold(input, output, length, x0, alpha);
-  double epsilon;
+  double epsilon = 0.0;
   for (size_t i = 0; i < length; i++) {
     double error = output[i] - expected[i];
     epsilon += error * error;
@@ -37,7 +37,7 @@ bool test_hard_threshold() {
   double expected[6] = { 0.0, 1.0, 0.0, 0.0, 0.0, 1.0 };
   double x0 = 2.0;
   hard_threshold(input, output, length, x0);
-  double epsilon;
+  double epsilon = 0.0;
   for (size_t i = 0; i < length; i++) {
     double error = output[i] - expected[i];
     epsilon += error * error;
@@ -55,7 +55,7 @@ bool test_hard_threshold_mul_invth() {
   double x0 = 1.0;
   double x1 = 3.0;
   hard_threshold_mul_invth(input, output, length, x0, x1);
-  double epsilon;
+  double epsilon = 0.0;
   for (size_t i = 0; i < length; i++) {
     double error = output[i] - expected[i];
     epsilon += error * error;
@@ -73,7 +73,7 @@ bool test_hard_threshold_mul_invth_array() {
   double x0[6] = {1.0, 2.0, 3.0, 0.0, 0.0, 6.0};
   double x1[6] = {3.0, 3.0, 4.0, 4.0, 1.0, 7.0};
   hard_threshold_mul_invth_array(input, output, length, x0, x1);
-  double epsilon;
+  double epsilon = 0.0;
   for (size_t i = 0; i < length; i++) {
     double error = output[i] - expected[i];
     epsilon += error * error;
@@ -83,12 +83,31 @@ bool test_hard_threshold_mul_invth_array() {
   return true;
 }
 
+bool test_linearized_threshold() {
+  size_t length = 6;
+  double input[6] = {1.2, 2.2, -1.2, 0.2, 0.9, 6.4};
+  double output[6] = {0};
+  double expected[6] = {0.56666667, 0.9, 0.0, 0.23333333, 0.46666667, 1.0 };
+  double x0 = 1.0;
+  double alpha = 3.0;
+  linearized_threshold(input, output, length, x0, alpha);
+  double epsilon = 0.0;
+  for (size_t i = 0; i < length; i++) {
+    double error = output[i] - expected[i];
+    epsilon += error * error;
+  }
+  assert( epsilon < 0.00001);
+  printf("All linearized_threshold tests passed.\n");
+  return true;
+}
+
 int main(int argc, char **argv) {
   test_clamp2();
   test_logistic_threshold();
   test_hard_threshold();
   test_hard_threshold_mul_invth();
   test_hard_threshold_mul_invth_array();
+  test_linearized_threshold();
   printf("All tests passed\n");
 }
 
